bb61f19d95a37fc4622c8370c369907d4aede1b4
Iqbal Fadhil <halo@iqbalfadhil.com>
2025-04-26 02:12:08 +0700

    Created routes

---
+++ routes/postRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../middleware/authMiddleware");
+const {
+  createPost, getPosts, getPost, updatePost, deletePost
+} = require("../controllers/postController");
+
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", auth, createPost);
+router.put("/:id", auth, updatePost);
+router.delete("/:id", auth, deletePost);
+
+module.exports = router;
